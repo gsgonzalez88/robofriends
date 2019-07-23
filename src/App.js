99f564aa868ed891dbca2f0fe4e84ceb9df7245b
gsgonzalez88 <gsgonzalez88@gmail.com>
2019-07-23 13:16:54 -0300

    Added the filter robots functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox.jsx';
 import { robots } from './robots';
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robots: robots,
+      searchField: ''
+    };
+  };
+
+  onSearchChange = (e) => {
+    this.setState({ searchField: e.target.value});
+        
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+        return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox
+          searchChange={this.onSearchChange}
+          searchField={this.state.searchField}
+        />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const SearchBox = () => {
-  return <div className="pa2">
-      <input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="Search robot" />
-  </div>;
+const SearchBox = ({ searchField, searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="Search robot"
+        onChange={searchChange}
+      />
+    </div>
+  );
 };
 
 export default SearchBox;
