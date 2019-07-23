bc66e078627c1407bb745e39b2227eea18af7444
gsgonzalez88 <gsgonzalez88@gmail.com>
2019-07-23 01:15:29 -0300

    added a CardList wraper for the Card component

---
+++ src/CardList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  const cardComponent = robots.map((robot) => {
+    return (
+      <Card
+        key={robot.id}
+        id={robot.id}
+        name={robot.name}
+        email={robot.email}
+      />
+    );
+  });
+  return <div>{cardComponent}</div>;
+};
+
+export default CardList;
+
+
+/* const CardList = ({ robots }) => {
+    const cardComponent = robots.map((user, i) => {
+      return (
+        <Card
+          key={robots[i].id}
+          id={robots[i].id}
+          name={robots[i].name}
+          email={robots[i].email}
+        />
+      );
+    });
+    return <div>{cardComponent}</div>;
+  };
+  
+  export default CardList; */
